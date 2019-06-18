ab3421a8345efb8e2058d6610f186ec4a2a00109
QiuLei <896275756@qq.com>
2019-06-19 00:46:00 +0800

    优化db_notes_async

--- management/commands/db_notes_async.py
+++ management/commands/db_notes_async.py
@@ -4,7 +4,6 @@
 将django models中注释，同步到mysql数据库中
 """
 import os
-import re
 from django.db.models import ManyToOneRel, OneToOneRel, UUIDField, DateTimeField, FloatField, GenericIPAddressField, \
     IntegerField, CharField, DecimalField, DateField, ManyToManyRel
 from django.db import connections
@@ -13,25 +12,12 @@ from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db.models.fields.reverse_related import ForeignObjectRel
 
-VALID_MODEL_APPS = []
+INSTALLED_VALID_APPS = []
 
-error_sqls = []
+error_info = []
 
 app_src_dir = os.path.dirname(settings.BASE_DIR)
 cursor = connections['default'].cursor()
-
-
-def check_contain_chinese(origin_str):
-    origin_str = origin_str.__str__()
-    zh_model = re.compile(u'[\u4e00-\u9fa5]')
-    try:
-        match = zh_model.search(origin_str)
-    except:
-        match = False
-    if match:
-        return True
-    else:
-        return False
 
 
 class DbNotesAsync(BaseCommand):
@@ -40,24 +26,21 @@ class DbNotesAsync(BaseCommand):
     def field_info(cls, field):
         """获取字段信息"""
         field_decimal_places = None
+        field_length = None
         if isinstance(field, UUIDField):
             field_type = 'char'
             field_length = 32
         elif isinstance(field, DateTimeField):
             field_type = 'datetime'
-            field_length = 6
         elif isinstance(field, DateField):
             field_type = 'date'
-            field_length = 0
         elif isinstance(field, FloatField):
-            field_type = 'double'
-            field_length = 0
+            field_type = 'float'
         elif isinstance(field, GenericIPAddressField):
             field_type = 'char'
             field_length = 39
         elif isinstance(field, IntegerField):
             field_type = 'int'
-            field_length = field.default if field.default and isinstance(field.default, int) else 11
         elif isinstance(field, CharField):
             field_type = 'varchar'
             field_length = field.max_length
@@ -67,7 +50,7 @@ class DbNotesAsync(BaseCommand):
             field_decimal_places = field.decimal_places
         else:
             field_type = 'invalid'
-            field_length = 999
+            field_length = -9999999     # 表示无效的值
 
         return field_type, field_length, field_decimal_places
 
@@ -78,33 +61,30 @@ class DbNotesAsync(BaseCommand):
         """
         for app_name in settings.INSTALLED_APPS:
             if os.path.exists('{}/{}/models.py'.format(app_src_dir, app_name)):
-                VALID_MODEL_APPS.append(app_name)
-        content_types = ContentType.objects.filter(app_label__in=VALID_MODEL_APPS).order_by('id')
+                INSTALLED_VALID_APPS.append(app_name)
+        content_types = ContentType.objects.filter(app_label__in=INSTALLED_VALID_APPS).order_by('id')
 
         for item in content_types:
             model_obj = item.model_class()
             if model_obj:
                 db_table = model_obj._meta.db_table
-                print('正在修改表{}'.format(db_table))
-                cls.add_table_notes_to_db(db_table=db_table, table_note=model_obj.__doc__)
+                table_note = model_obj._meta.verbose_name or model_obj.__doc__
+                cls.add_table_notes_to_db(db_table=db_table, table_note=table_note)
                 for field in model_obj._meta.get_fields():
-                    if not (isinstance(field, ManyToOneRel) or
-                            isinstance(field, OneToOneRel) or
-                            isinstance(field, UUIDField) or
-                            isinstance(field, ManyToManyRel) or
-                            isinstance(field, ForeignObjectRel)
+                    if not (isinstance(field, ManyToOneRel) or isinstance(field, OneToOneRel)
+                            or isinstance(field, UUIDField) or isinstance(field, ManyToManyRel)
+                            or isinstance(field, ForeignObjectRel)
                     ) and field.help_text:
-                        if check_contain_chinese(field.help_text):
-                            field_type, field_length, field_decimal_places = cls.field_info(field)
-                            if field_length == 999:
-                                continue
-                            cls.add_field_notes_to_db(db_table=db_table, column=field.name,
-                                                      field_type=field_type, field_length=field_length,
-                                                      field_decimal_places=field_decimal_places,
-                                                      comment=field.help_text)
-        for sql in error_sqls:
-            print('altert table column error: ', sql)
-        return VALID_MODEL_APPS
+                        field_type, field_length, field_decimal_places = cls.field_info(field)
+                        if field_length == -9999999:
+                            continue
+                        cls.add_field_notes_to_db(db_table=db_table, column=field.name,
+                                                  field_type=field_type, field_length=field_length,
+                                                  field_decimal_places=field_decimal_places,
+                                                  comment=field.help_text)
+        for item in error_info:
+            print('错误信息: ', item)
+        return INSTALLED_VALID_APPS
 
     @staticmethod
     def add_table_notes_to_db(db_table, table_note):
@@ -114,17 +94,18 @@ class DbNotesAsync(BaseCommand):
             sql = """alter table `{db_table}` comment "{table_note}" """.format(db_table=db_table, table_note=table_note)
             try:
                 cursor.execute(sql)
-            except:
-                print('alter table error: ', sql)
+            except Exception as e:
+                print('修改表{}发生异常，注释为为: {}，异常原因: {}'.format(db_table, table_note, e))
+            print('成功修改表: 【{}】, 添加备注为: 【{}】......'.format(db_table, table_note))
         return
 
     @staticmethod
     def add_field_notes_to_db(db_table, column, field_type, field_length, comment, field_decimal_places=None):
         """添加字段注释"""
-        if column in ['created_at', 'updated_at', 'created_timestamp']:
+        if not comment:
             return
-        if field_type in ['date', 'double']:
-            column_data = '`{column}` {field_type} default null'.format(column=column, field_type=field_type)
+        if field_type in ['int', 'double', 'float', 'date', 'datetime']:
+            column_data = '`{column}` {field_type}'.format(column=column, field_type=field_type)
         elif field_type == 'decimal' and field_decimal_places:
             column_data = '`{column}` {field_type}({field_length}, {field_decimal_places})'.\
                 format(column=column, field_type=field_type, field_length=field_length,
@@ -137,8 +118,10 @@ class DbNotesAsync(BaseCommand):
             format(db_table=db_table, column_data=column_data, comment=comment)
         try:
             cursor.execute(sql)
-        except:
-            error_sqls.append(sql)
+        except Exception as e:
+            error_info.append('表: {}, 字段: {}, 注释信息: {}, sql: {}，原因: {}'.format(db_table, column,
+                                                                               comment, sql, e))
+        print('表:【{}】, 字段: 【{}】, 注释信息: 【{}】, 添加成功......'.format(db_table, column, comment))
         return
 
     def handle(self, *args, **options):
